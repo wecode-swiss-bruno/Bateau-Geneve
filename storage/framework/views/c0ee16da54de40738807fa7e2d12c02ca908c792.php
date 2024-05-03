90f24586f4e835bc3ffe49a287928710d5aace77
Bruno <bruno@wecode.swiss>
2024-05-03 11:43:26 +0200

    after merge

--- storage/framework/views/c0ee16da54de40738807fa7e2d12c02ca908c792.php
+++
@@ -1,7 +0,0 @@
-<div
-    class="folder-tree"
-    data-tree-data='<?php echo json_encode($folderTree, 15, 512) ?>'
-    data-folders='<?php echo json_encode($folderList, 15, 512) ?>'>
-    <button>Go to root folder</button>
-</div>
-<?php /**PATH /Users/vladtemneanu/Library/CloudStorage/SynologyDrive-Wecode/WECODE-SHARE/_COLLABORATEURS/VLAD/LOCAL DEV/Bateau-Geneve/app/main/widgets/mediamanager/folder_tree.blade.php ENDPATH**/ ?>
--- storage/framework/views/c82d1647401ec254b586bd3595d4a32fe5a279f4.php
+++
@@ -1,9 +0,0 @@
-<input type="hidden" data-media-type="current-folder" value="<?php echo e($currentFolder); ?>"/>
-
-<?php if($items): ?>
-    <?php echo $this->makePartial('mediamanager/list_grid'); ?>
-
-<?php else: ?>
-    <p><?php echo app('translator')->get('admin::lang.text_empty'); ?></p>
-<?php endif; ?>
-<?php /**PATH /Users/vladtemneanu/Library/CloudStorage/SynologyDrive-Wecode/WECODE-SHARE/_COLLABORATEURS/VLAD/LOCAL DEV/Bateau-Geneve/app/main/widgets/mediamanager/item_list.blade.php ENDPATH**/ ?>
